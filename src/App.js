0ac101ec9a4ad7cc70667f1377311edbe59b5f1f
Frățilă Valeriu-Matei <33498828+Matei-Fratila@users.noreply.github.com>
2023-12-09 19:44:43 +0200

    Test async data

--- src/App.js
+++ src/App.js
@@ -20,9 +20,22 @@ const initialStories = [
   },
 ];
 
+const getAsyncStories = () =>
+  new Promise(resolve =>
+    setTimeout(() =>
+      resolve({ data: { stories: initialStories } }),
+      2000)
+  );
+
 const App = () => {
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
-  const [stories, setStories] = React.useState(initialStories);
+  const [stories, setStories] = React.useState([]);
+
+  React.useEffect(() => {
+    getAsyncStories().then(result => {
+      setStories(result.data.stories);
+    });
+  }, []);
 
   const handleRemoveStory = item => {
     alert(item.objectID);
