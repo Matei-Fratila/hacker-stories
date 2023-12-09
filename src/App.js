4b9a99b2953ac068d4d726b8b8c4742bace4b4cb
Frățilă Valeriu-Matei <33498828+Matei-Fratila@users.noreply.github.com>
2023-12-09 19:55:00 +0200

    Display Loading... and error message

--- src/App.js
+++ src/App.js
@@ -30,11 +30,15 @@ const getAsyncStories = () =>
 const App = () => {
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
   const [stories, setStories] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
 
   React.useEffect(() => {
+    setIsLoading(true);
     getAsyncStories().then(result => {
       setStories(result.data.stories);
-    });
+      setIsLoading(false);
+    }).catch(() => setIsError(true));
   }, []);
 
   const handleRemoveStory = item => {
@@ -63,7 +67,8 @@ const App = () => {
         <strong>Search:</strong>
       </InputWithLabel>
       <hr />
-      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+      {isError && <p>Something went wrong</p>}
+      {isLoading ? (<p>Loading...</p>) : (<List list={searchedStories} onRemoveItem={handleRemoveStory} />)}
     </div>
   )
 };
