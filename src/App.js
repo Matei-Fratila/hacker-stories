d527443a462421de75480206dd2e22090b4b3e52
Frățilă Valeriu-Matei <33498828+Matei-Fratila@users.noreply.github.com>
2023-12-11 22:02:09 +0200

    Created search form component

--- src/App.js
+++ src/App.js
@@ -76,8 +76,9 @@ const App = () => {
     setSearchTerm(event.target.value);
   }
 
-  const handleSearchSubmit = () => {
+  const handleSearchSubmit = event => {
     setUrl(`${API_ENDPOINT}${searchTerm}`);
+    event.preventDefault();
   }
 
   const handleFetchStories = React.useCallback(async () => {
@@ -112,26 +113,38 @@ const App = () => {
   return (
     <div>
       <h1>My hacker stories</h1>
-      <InputWithLabel
-        id="search"
-        isFocused={true}
-        value={searchTerm}
-        onInputChange={handleSearchInput}>
-        <strong>Search:</strong>
-      </InputWithLabel>
-      <button
-        type="button"
-        disabled={!searchTerm}
-        onClick={handleSearchSubmit}
-      >
-        Submit
-      </button>
+
+      <SearchForm
+        searchTerm={searchTerm}
+        onSearchInput={handleSearchInput}
+        onSearchSubmit={handleSearchSubmit}
+      />
+      
       <hr />
       {stories.isError && <p>Something went wrong</p>}
       {stories.isLoading ? (<p>Loading...</p>) : (<List list={stories.data} onRemoveItem={handleRemoveStory} />)}
     </div>
   )
 };
+
+const SearchForm = ({
+  searchTerm,
+  onSearchInput,
+  onSearchSubmit,
+}) => (
+  <form onSubmit={onSearchSubmit}>
+    <InputWithLabel
+      id="search"
+      isFocused={true}
+      value={searchTerm}
+      onInputChange={onSearchInput}>
+      <strong>Search:</strong>
+    </InputWithLabel>
+    <button type="submit" disabled={!searchTerm}>
+      Submit
+    </button>
+  </form>
+)
 
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = React.useState(
